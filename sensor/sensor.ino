2acf72d72b49b4da8ed16ad43b0502a7fefa6224
mrr79 <marianarojas2197@gmail.com>
2024-04-04 08:05:36 -0600

    sensor, leds, y mapeo 10/10

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,4 +1,13 @@
-// Declaración de arreglos y variables globales
+//#include <LiquidCrystal.h>
+
+//Lecturas de las señales retroalimentadas 
+//const int binFB0 = A5;
+//const int binFB1 = A4;
+//const int binFB2 = A3;
+
+// select the pins used on the LCD panel
+//LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
 int numBin[2]; // Arreglo para almacenar la representación binaria del número
 int numGrey[2];      // Arreglo para almacenar la representación de código Gray
 int comparacion;  // Variable para realizar la suma de bits binarios
@@ -11,7 +20,12 @@ void setup(void) {
   // Inicialización de comunicación serial
   Serial.begin(9600);
 
-   // Configuración del sensor de ultrasonido
+  //Pines de entrada de la retroalimentación
+  //pinMode(binFB0, INPUT);
+  //pinMode(binFB1, INPUT);
+  //pinMode(binFB2, INPUT);
+
+  // Configuración del sensor de ultrasonido
   pinMode(Trigger, OUTPUT); // Pin como salida para el Trigger
   pinMode(Echo, INPUT);     // Pin como entrada para el Echo
 
@@ -20,6 +34,30 @@ void setup(void) {
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
 }
+
+/*void LCD(int numero, int entrada){
+  if (entrada >= 8){
+    lcd.begin(16, 2);              // start the library
+    lcd.setCursor(0,0);
+    lcd.print("Hexadecimal"); 
+    lcd.setCursor(14,0);
+    lcd.print(0);              
+    lcd.setCursor(0,1);
+    lcd.print("Gray "); 
+    lcd.setCursor(14,1);
+    lcd.print(0);
+  }else{
+    lcd.begin(16, 2);              // start the library
+  lcd.setCursor(0,0);
+  lcd.print("Hexadecimal"); 
+  lcd.setCursor(14,0);
+  lcd.print(numero); 
+  lcd.setCursor(0,1);
+  lcd.print("Gray "); 
+  lcd.setCursor(14,1);
+  lcd.print(entrada);
+  }
+}*/
 
 // Función para convertir un número a su representación binaria
 void Binario(int numero) {
@@ -68,6 +106,7 @@ void Binario(int numero) {
   }
 }
 
+
 // Función para escribir en los pines digitales según la representación de código Gray
 void escribirCompuerta() {
   for (int i = 0; i < 3; i++) {
@@ -80,13 +119,11 @@ void escribirCompuerta() {
 }
 
 // Función para convertir la representación binaria a código Gray
-// Función para convertir la representación binaria a código Gray
 void Grey() {
   numGrey[0] = numBin[0];
-  numGrey[1] = numBin[0] ^ numBin[1];  // XOR entre el primer y segundo bit
-  numGrey[2] = numBin[1] ^ numBin[2];  // XOR entre el segundo y tercer bit
+  numGrey[1] = sumaBinario(numBin[0], numBin[1]);
+  numGrey[2] = sumaBinario(numBin[1], numBin[2]);
 }
-
 
 // Función para sumar dos bits binarios y obtener el resultado
 int sumaBinario(int num1, int num2) {
@@ -100,10 +137,17 @@ int sumaBinario(int num1, int num2) {
 }
 
 void loop() {
+  //Leer el valor de la retroalimentación}
+  //int bitFB2 = digitalRead(binFB2);
+  //int bitFB1 = digitalRead(binFB1);
+  //int bitFB0 = digitalRead(binFB0);
+
+  //int hexadecimal = ((bitFB2 * 4) + (bitFB1 * 2) + (bitFB0 * 1));
+
   // Genera un pulso corto en el pin de Trigger del sensor de ultrasonido
   digitalWrite(Trigger, LOW);
   delayMicroseconds(2);
-  digitalWrite(Trigger, HIGH);   // Genera el pulso de trig por 10ms
+  digitalWrite(Trigger, HIGH);
   delayMicroseconds(10);
   digitalWrite(Trigger, LOW);
 
@@ -114,36 +158,39 @@ void loop() {
   int distancia = (duracion / 2) / 29; // Calcula la distancia en centímetros
 
   // Limitar la distancia a 80 cm
-  if (distancia > 80) {
-    distancia = 80;
+  if (distancia > 40) {
+    distancia = 40;
   }
 
+  // Impresión de la distancia por serial
+
+
+  // Mapeo de la distancia a un rango entre 0 y 7
   Serial.print("Distancia: ");
   Serial.print(distancia); // Envía el valor de la distancia por el puerto serie
 
   // Convertir la distancia a la sección correspondiente
-  int section = map(distancia, 0, 80, 0, 7);
+  int section = map(distancia, 0, 40, 0, 7);
 
-  // Impresión de la sección decimal y binaria por serial
-  Serial.print(" Sección decimal: ");
-  Serial.print(section);
-  Serial.print(" (binario: ");
-  Serial.print(section / 4);
-  section %= 4;
-  Serial.print((section / 2) % 2);
-  Serial.print(section % 2);
-  Serial.print(") ");
+
+  //Mostrar en LCD
+  //LCD(hexadecimal, redondeo);
+  // Conversión del número redondeado a su representación binaria
+  Binario(section);
+
+  // Impresión del número redondeado por serial
+  Serial.println(section);
+  Serial.println("binario");
+  Serial.println(numBin[0]); // Impresión del primer bit binario
+  Serial.println(numBin[1]); // Impresión del segundo bit binario
+  Serial.println(numBin[2]); // Impresión del tercer bit binario
 
   // Conversión de la representación binaria a código Gray
   Grey();
-
-  // Impresión de la sección en código Gray binario y su representación binaria por serial
-  Serial.print("Sección en código Gray binario: ");
-  Serial.print(numGrey[0]);
-  Serial.print(numGrey[1]);
-  Serial.print(numGrey[2]);
-  Serial.print("  ______");
-  Serial.println();
+  Serial.println("grey");
+  Serial.println(numGrey[0]); // Impresión del A de código Gray
+  Serial.println(numGrey[1]); // Impresión del B de código Gray
+  Serial.println(numGrey[2]); // Impresión del C de código Gray 
 
   // Escritura en los pines digitales según la representación de código Gray
   escribirCompuerta();
